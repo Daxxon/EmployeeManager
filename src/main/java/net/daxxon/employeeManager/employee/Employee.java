04ad7d52e0102f8462e02997d170c0c8da63ded2
Daniel Waggoner <daxxon37@hotmail.com>
2018-02-28 22:18:55 -0500

    added basic employee

---
+++ src/main/java/net/daxxon/employeeManager/employee/Employee.java
@@ -0,0 +1,50 @@
+package net.daxxon.employeeManager.employee;
+
+public class Employee {
+  private int id;
+  private String firstName;
+  private String lastName;
+  private String position;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getPosition() {
+    return position;
+  }
+
+  public void setPosition(String position) {
+    this.position = position;
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" +
+        "id=" + id +
+        ", firstName='" + firstName + '\'' +
+        ", lastName='" + lastName + '\'' +
+        ", position='" + position + '\'' +
+        '}';
+  }
+}
